78984f5a2e2746467267ac1e93fb911dca635031
Guillaume <guillaumeargensse@hotmail.fr>
2024-06-13 17:22:12 +0200

    resolving issue#4, refactoring code, adding new form element in html, adding new css rules to this form, adding functions parseAllAlphaNumField and displayConfirmPopup, modified function validateRadio and validate, deleted RegExp dateReg, modified textReg and mailReg regexp in modal.js

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -7,11 +7,6 @@ function editNav() {
   }
 }
 
-// RegExp
-const textReg = new RegExp("^[a-zA-Z\-]{2,}");
-const mailReg = new RegExp("^[a-zA-Z0-9._\-]+\@[a-zA-Z]+\.[a-zA-Z]+");
-const dateReg = new RegExp("^[0-9]{2}\/[0-9]{2}\/[0-9]{4}$");
-const quantityReg = new RegExp("^[0-9]{1,2}$");
 
 // DOM Elements
 const modalbg = document.querySelector(".bground");
@@ -36,10 +31,28 @@ function closeModal() {
 }
 
 /****************** Fonctions de validation des champs du formulaire ********************/
+
+// RegExp
+const textReg = new RegExp("^[A-Za-zÀ-ÖØ-öø-ÿ\-]{2,}");
+const mailReg = new RegExp("^[A-Za-zÀ-ÖØ-öø-ÿ0-9._\-]+\@[a-zA-Z]+\.[a-zA-Z]+");
+const quantityReg = new RegExp("^[0-9]{1,2}$");
+
+// DOM Elements
+const everyAlphaNumFields = document.querySelectorAll(
+  "input[type=text], input[type=email], input[type=date], input[type=number]");
+const everyBtnRadio = document.querySelectorAll("input[type=radio]");
+const requiredCheckboxBtn = document.getElementById("checkbox1");
+const confirmPopup = document.getElementById("valid");
+
+// Display popup form
+function displayConfirmPopup() {
+  confirmPopup.style.display = "flex";
+}
+
 /**
  * Cette fonction récupère la valeur de l'element HTML
  * et la compare à l'expression régulière textReg
- * Si le test est false, on ajoute une paire d'attribut à l'element parent
+ * Si le test est false, ajoute une paire d'attribut à l'element parent
  * pour afficher un message d'avertissement et retourne false
  * @param {HTMLElement} input 
  * @returns bool
@@ -59,7 +72,7 @@ function validateName(input) {
 /**
  * Cette fonction récupère la valeur de l'element HTML
  * On compare avec l'expression régulière mailReg
- * Si le test est false, on ajoute une paire d'attribut à l'element parent
+ * Si le test est false, ajoute une paire d'attribut à l'element parent
  * pour afficher un message d'avertissement et retourne false
  * @param {HTMLElement} input 
  * @returns bool
@@ -76,17 +89,11 @@ function validateEmail(input) {
   }
 }
 
-// var inputDate = document.querySelectorAll("input[type=date]");
-// for (let d of inputDate) {
-//   d.type='';
-// }
-
 /**
  * Cette fonction récupère la valeur de l'element HTML et on construit une 
- * instance Date avec cette valeur. La fonction test si date est un nombre
- * (si date est de type string ou null, NaN retourne true). Si isNaN retourne
- * true, la date est invalide, on ajoute une paire d'attribue à l'element parent
- * pour afficher un message d'avertissement et on retourne false
+ * instance Date "date" avec cette valeur. La fonction test si date est un nombre.
+ * Si isNaN retourne true, la date est invalide, ajoute une paire d'attribut
+ * à l'element parent pour afficher un message d'avertissement et on retourne false
  * @param {HTMLElement} input 
  * @returns bool
  */
@@ -104,9 +111,9 @@ function validateDate(input) {
 
 /**
  * Cette fonction récupère la valeur d'un element HTML et la compare avec
- * l'expression régulière quantityReg. Si le test retourne false, on ajoute
+ * l'expression régulière quantityReg. Si le test retourne false,ajoute
  * une paire d'attribut à l'element parent pour afficher un message d'avertissement
- * et on retourne false
+ * et retourne false
  * @param {HTMLElement} input 
  * @returns bool
  */
@@ -123,29 +130,54 @@ function validateQuantity(input) {
 }
 
 /**
- * Cette fonction récupère l'ensemble des boutons radios du document et
- * stock dans la variable radioChecked le bouton radio "checked". Si
- * radioChecked est null, aucun bouton radio n'est selectionné, on ajoute
- * donc une paire d'attribut à l'element parent pour afficher un message
- * d'avertissement et on retourne false
+ * Cette fonction récupère le type d'input et appel la fonction voulue
+ * selon le type pour valider l'input
+ * @param {HTMLElement} input input de type text, email, date ou number
  * @returns bool
  */
-function validateRadio() {
-  let radioBtn = document.querySelectorAll("input[type=radio");
-  let radioChecked = document.querySelector("input[type=radio]:checked");
+function parseAllAlphaNumField(input) {
+  switch (input.getAttribute("type")) {
+    case "text":
+      return validateName(input);
+    case "email":
+      return validateEmail(input);
+    case "date":
+      return validateDate(input);
+    case "number":
+      return validateQuantity(input);
+    default:
+      return false;
+  }
+}
+
+/**
+ * Cette fonction récupère le bouton radio checked dans la nodelist des boutons radios.
+ * S'il n'y en a pas, renvoie false et ajoute une paire d'attribut à l'element 
+ * parent pour afficher un message d'avertissement
+ * @param {NodeListOf<Element>} allRadioBtn nodelist de l'ensemble des boutons radios
+ * @returns bool
+ */
+function validateRadio(allRadioBtn) {
+  let radioChecked = null;
+  for(let i = 0; i < allRadioBtn.length; i++) {
+    if (allRadioBtn[i].checked) {
+      radioChecked = allRadioBtn[i];
+    }
+  }
   if (radioChecked != null) {
     radioChecked.parentElement.setAttribute("data-error-visible", "false");
     return true;
   } else {
-    radioBtn[0].parentElement.setAttribute("data-error", "Veuillez selectionner une ville");
-    radioBtn[0].parentElement.setAttribute("data-error-visible", "true");
+    allRadioBtn[0].parentElement.setAttribute("data-error", "Veuillez selectionner une ville");
+    allRadioBtn[0].parentElement.setAttribute("data-error-visible", "true");
     return false;
   }
 }
 
 /**
- * Cette fonction test si une checkbox est checked. Si c'est faux,
- * on affiche un message d'avertissement
+ * Cette fonction test si la checkbox requise est checked. Si c'est faux,
+ * ajoute une paire d'attribut à l'element parent pour afficher un message d'avertissement
+ * et retourne false
  * @param {HTMLElement} input 
  * @returns bool
  */
@@ -163,44 +195,42 @@ function isREquiredChecked(input) {
 /**
  * Cette fonction s'assure que toutes les valeurs du tableau
  * array soit true
- * @param {array} array 
+ * @param {boolean} array liste de valeur bool de l'ensemble des tests sur les inputs
+ * @returns boolean
  */
 function isAllTrue(array) {
   if (array.every((elem) => elem === true)){
-    console.log("valid !");
+    return true;
+  } else {
+    return false;
   }
 }
 
 /************** Event listener sur submit et fonction validate() associée **************/
 /**
- * Cette fonction est appelée lors du click sur l'input type submit dans form
- * Elle récupère les elements HTML des différents champs du formulaire et les test
- * Construit la liste des résultats des test (liste de booléen)
+ * Cette fonction construit l'array boolFromValidation avec le résultat des validations
+ * de l'ensemble des inputs et affiche le popup de confirmation si tout est validé
+ * @param {NodeListOf<Element>} alphaNumFields nodelist des inputs alphanumérique
+ * @param {NodeListOf<Element>} btnRadio nodelist des inputs radio
+ * @param {HTMLElement} btnRequired checkbox checked requise
  */
-function validate() {
-  // Initialisation
-  let listValidation = [];
-  let inputFirst = document.getElementById("first");
-  let inputLast = document.getElementById("last");
-  let inputEmail = document.getElementById("email");
-  let inputDate = document.getElementById("birthdate");
-  let inputParticipation = document.getElementById("quantity");
-  let btnRequired = document.getElementById("checkbox1");
+function validate(alphaNumFields, btnRadio, btnRequired) {
 
-  // Test des champs et construction de la liste
-  listValidation.push(validateName(inputFirst));
-  listValidation.push(validateName(inputLast));
-  listValidation.push(validateEmail(inputEmail));
-  listValidation.push(validateDate(inputDate));
-  listValidation.push(validateQuantity(inputParticipation));
-  listValidation.push(validateRadio());
-  listValidation.push(isREquiredChecked(btnRequired));
+  let boolFromValidation = [];
 
-  isAllTrue(listValidation);
+  for(let i = 0; i < alphaNumFields.length; i++) {
+    boolFromValidation.push(parseAllAlphaNumField(alphaNumFields[i]));
+  }
+  boolFromValidation.push(validateRadio(btnRadio));
+  boolFromValidation.push(isREquiredChecked(btnRequired));
+
+  if (isAllTrue(boolFromValidation)) {
+    displayConfirmPopup();
+  }
 }
 
 let formSubmit = document.querySelector("form");
 formSubmit.addEventListener("submit", (event) => {
   event.preventDefault();
-  validate();
+  validate(everyAlphaNumFields, everyBtnRadio, requiredCheckboxBtn);
 })
